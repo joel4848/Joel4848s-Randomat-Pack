cfd1648045d65488436616c60a3f1217cef3293b
Joel Taylor <joel.w.taylor@gmail.com>
2026-01-16 11:38:50 +0000

    I think I fixed individual timers in maljumption? Can't remember, and can't test with bots.

--- lua/randomat2/events/maljumption.lua
+++ lua/randomat2/events/maljumption.lua
@@ -13,37 +13,75 @@ CreateConVar("randomat_maljumption_affectall", 1, FCVAR_NONE, "Set to 1 for the 
 function EVENT:Begin()
     local lower = GetConVar("randomat_maljumption_lower"):GetInt()
     local upper = GetConVar("randomat_maljumption_upper"):GetInt()
-
+    local affectall = GetConVar("randomat_maljumption_affectall"):GetBool()
+    
     -- For sanity (Thanks Mal)
     if lower > upper then
         upper = lower + 1
     end
-
-    local x = 0
-    timer.Create("RdmtMaljumptionMain", math.random(lower, upper), 0, function()
+    
+    self.MaljumptionTimers = self.MaljumptionTimers or {}
+    
+    local function ForceJump(ply)
+        if not IsValid(ply) or not ply:Alive() or ply:IsSpec() then return end
+        
+        ply:ConCommand("+jump")
+        timer.Simple(0.1, function()
+            if IsValid(ply) then
+                ply:ConCommand("-jump")
+            end
+        end)
+    end
+    
+    -- affectall = 1 global timer
+    if affectall then
+        timer.Create("RdmtMaljumptionMain", math.random(lower, upper), 0, function()
+            for _, ply in ipairs(self:GetAlivePlayers(true)) do
+                ForceJump(ply)
+            end
+            
+            timer.Adjust("RdmtMaljumptionMain", math.random(lower, upper))
+        end)
+        return
+    end
+    
+    -- affectall = 0 individual timers
+    timer.Create("RdmtMaljumptionIndividual", 1, 0, function()
         for _, ply in ipairs(self:GetAlivePlayers(true)) do
-            if x == 0 or GetConVar("randomat_maljumption_affectall"):GetBool() then
-                if IsValid(ply) and ply:Alive() then
-                    -- Force press & release jump... maybe there's a neater way of doing this?
-                    ply:ConCommand("+jump")
-                    timer.Simple(0.1, function()
-                        if IsValid(ply) then
-                            ply:ConCommand("-jump")
-                        end
-                    end)
-
-                    x = 1
-                end
+            if not self.MaljumptionTimers[ply] then
+                local timerName = "RdmtMaljumption_" .. ply:SteamID64()
+                self.MaljumptionTimers[ply] = timerName
+                
+                timer.Create(timerName, math.random(lower, upper), 0, function()
+                    if not IsValid(ply) or not ply:Alive() or ply:IsSpec() then
+                        timer.Remove(timerName)
+                        self.MaljumptionTimers[ply] = nil
+                        return
+                    end
+                    
+                    ForceJump(ply)
+                    timer.Adjust(timerName, math.random(lower, upper))
+                end)
             end
         end
-
-        x = 0
-        timer.Adjust("RdmtMaljumptionMain", math.random(lower, upper))
     end)
 end
 
 function EVENT:End()
+    
     timer.Remove("RdmtMaljumptionMain")
+    timer.Remove("RdmtMaljumptionIndividual")
+    
+    if self.MaljumptionTimers then
+        for ply, timerName in pairs(self.MaljumptionTimers) do
+            timer.Remove(timerName)
+            if IsValid(ply) then
+                ply:ConCommand("-jump")
+            end
+        end
+    end
+    
+    self.MaljumptionTimers = nil
 end
 
 function EVENT:GetConVars()
@@ -61,7 +99,7 @@ function EVENT:GetConVars()
             })
         end
     end
-
+    
     local checks = {}
     for _, v in ipairs({"affectall"}) do
         local name = "randomat_" .. self.id .. "_" .. v
