f9e9dabbed762f0321f51d360a1853e869c188dc
Joel Taylor <joel.w.taylor@gmail.com>
2026-01-23 15:29:16 +0000

    Corrected FCVARS

--- lua/randomat2/events/baitshark.lua
+++ lua/randomat2/events/baitshark.lua
@@ -27,12 +27,12 @@ util.AddNetworkString("rdmtSharkWinScreen")
 util.AddNetworkString("rdmtSharkWinScreenUnhook")
 
 
-CreateConVar("randomat_baitshark_harpoonAmount", 1, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Number of harpoons each traitor gets", 1, 10)
-CreateConVar("randomat_baitshark_killTraitorsOnEmpty", 0, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Whether traitors die after missing with all their harpoons", 0, 1)
-CreateConVar("randomat_baitshark_innocentSpeedMulti", 1, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "The innocent player's speed multiplier", 1, 2)
-CreateConVar("randomat_baitshark_makeInnocentShark", 1, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Whether to change the innocent's model to a shark", 0, 1)
-CreateConVar("randomat_baitshark_highlightInnocent", 0, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Whether the traitors can see the innocent through walls", 0, 1)
-CreateConVar("randomat_baitshark_traitorBlindDuration", 15, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "How long to blind & freeze traitors for", 0, 30)
+CreateConVar("randomat_baitshark_harpoonAmount", 1, {FCVAR_NONE}, "Number of harpoons each traitor gets", 1, 10)
+CreateConVar("randomat_baitshark_killTraitorsOnEmpty", 0, {FCVAR_NONE}, "Whether traitors die after missing with all their harpoons", 0, 1)
+CreateConVar("randomat_baitshark_innocentSpeedMulti", 1, {FCVAR_NONE}, "The innocent player's speed multiplier", 1, 2)
+CreateConVar("randomat_baitshark_makeInnocentShark", 1, {FCVAR_NONE}, "Whether to change the innocent's model to a shark", 0, 1)
+CreateConVar("randomat_baitshark_highlightInnocent", 0, {FCVAR_NONE}, "Whether the traitors can see the innocent through walls", 0, 1)
+CreateConVar("randomat_baitshark_traitorBlindDuration", 15, {FCVAR_NONE}, "How long to blind & freeze traitors for", 0, 30)
 
 
 -- Get available shark player models (Sharky looking scared with 'poon me' postit note body)
